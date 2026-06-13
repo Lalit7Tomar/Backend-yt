16ec5a20801f909aa5af84a5874ca365e89cf871
Lalit7Tomar <lalittomar0612@gmail.com>
2026-06-13 22:57:15 +0530

    updated user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -31,10 +31,12 @@ const userSchema = new mongoose.Schema({
         type:String,
 
     },
-    watchHistory:{
+    watchHistory:[
+        {
         type:Schema.Types.ObjectId,
         ref:"Video"
-    },
+        }
+    ],
     password: {
         type:String,
         required:[true,'Password is required']
@@ -46,10 +48,34 @@ const userSchema = new mongoose.Schema({
 // mongoose aggregate paginate,jwt and bcrypt
 userSchema.pre("save",async function (next) {
     if(!this.isModified("password")) return next();
-    this.password= bcrypt.hash(this.password, 10)
+    this.password=await bcrypt.hash(this.password, 10)
     next()
 })
 userSchema.methods.isPasswordCorrect =async function (password){
-    bcrypt.compare()
+   return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken =function(){
+    jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESSS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.ACCESSS_TOKEN_SECRET,{
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
 export const User=mongoose.model("User",userSchema)
